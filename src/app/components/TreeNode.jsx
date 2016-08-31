8ce60932110f165a2189fac92c5987e5c15f6630
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2016-08-31 09:32:57 +0800

    update

--- src/app/components/TreeNode.jsx
+++ src/app/components/TreeNode.jsx
@@ -1,15 +1,53 @@
 'use strict';
 
 
-import React, { PropTypes } from 'react'
+import React, { PropTypes } from 'react';
+import classNames from 'classnames';
+import { DragSource,DropTarget} from 'react-dnd';
+import * as Func from '../controls/DragFunc.jsx';
+
+const Types = {
+  TREE: 'tree'
+};
+
+@DragSource(Types.TREE,Func.source,Func.sourceCollect)
+@DropTarget(Types.TREE,Func.target,Func.targetCollect)
 
 export default class TreeNode extends React.Component {
+  state = {
+    collapsed:false
+  };
   render () {
+    let collapsedProps={
+            className:'arrow',
+            onClick:() => {
+              this.setState({
+                collapsed:!this.state.collapsed
+              })
+              }
+                };
+      let hasChild=this.props.node.get('Children') && this.props.node.get('Children').size > 0;
     return (
-      <div>
-        {this.props.get('Name')}
-        <div style={{paddingLeft: 20}}>
-          {this.props.children}
+      <div className="pop-tree-node-container">
+        <div className='tree-node'
+          style={{paddingLeft: 24+( this.props.paths.length - 1) * 11}}
+        title={this.props.node.get("Name")}>
+        <div {...collapsedProps}>
+          <div className={classNames({
+              "hasChild"  : hasChild,
+              "hasNoChild": !hasChild
+            })}>
+            <div className={classNames({
+                "fa icon-hierarchy-unfold": !this.state.collapsed,
+                "fa icon-hierarchy-fold"  : this.state.collapsed
+              })}/>
+            </div>
+          </div>
+          <div className="content" title={this.props.node.get("Name")}>{this.props.node.get('Name')}</div>
+        </div>
+
+        <div className='tree-children'>
+          {!this.state.collapsed &&this.props.children}
         </div>
       </div>
     )
@@ -17,5 +55,6 @@ export default class TreeNode extends React.Component {
 }
 
 TreeNode.propTypes = {
-  data:PropTypes.object,
-}
+  node:PropTypes.object,
+  paths:PropTypes.array,
+};
