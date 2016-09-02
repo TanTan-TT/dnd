8433cd25556ff4f1d25fe7793ee03205a3abdd36
WangyanhuiRea <yanhui.wang@schneider-electric.com>
2016-09-02 09:57:47 +0800

    fix drop bug

--- src/app/controls/DragFunc.jsx
+++ src/app/controls/DragFunc.jsx
@@ -58,13 +58,7 @@ export var target = {
     }
   },
   drop: function (props,monitor) {
-    console.log('over__drop');
-    console.log(monitor.getItem().id);
-    console.log(props.node.get('Name'));
-    if(props.node.get('childrenNum')===0){
       dragNode(monitor.getItem().id,props.node.get('Id'),null);
-    }
-
   }
 };
 export function targetCollect(connect, monitor) {
